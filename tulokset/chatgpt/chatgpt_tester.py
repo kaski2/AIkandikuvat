b67a7f3f6af1e43b78fab5a63b606393cf0e525d
MikoNikula <144808477+MikoNikula@users.noreply.github.com>
2025-04-15 22:16:07 +0300

    Chatgpt 4o mallin testaamiseen käytetty koodi

---
+++ tulokset/chatgpt/chatgpt_tester.py
@@ -0,0 +1,27 @@
+
+
+from openai import OpenAI
+
+client = OpenAI(api_key = OPENAI_API_KEY)
+
+prompt_txt = "Describe this image"
+img_Url = "https://github.com/kaski2/AIkandikuvat/blob/main/kuvatEnglanti/E_IMG6PEOPLE.jpg?raw=true"
+img_Name = "IMG6PEOPLE"
+
+response = client.responses.create(
+    model = "gpt-4o",
+    input = [{
+        "role": "user",
+        "content": [
+            {"type": "input_text", "text": prompt_txt},
+            {
+                "type": "input_image",
+                "image_url": img_Url,
+            },
+        ],
+    }],
+)
+
+print(response.output_text)
+
+print(img_Name)
